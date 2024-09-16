a52cedfc5860db7570c52822ed8233163d05a505
MatheusDLins <matheus934qwer@gmail.com>
2024-09-16 08:32:51 -0300

    chore: update application.yml and modify SpotifyConfig

---
+++ backend/src/main/java/com/example/spotify/config/SpotifyConfig.java
@@ -0,0 +1,13 @@
+package com.example.spotify.config;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "spotify")
+public class SpotifyConfig {
+    private String clientId;
+    private String clientSecret;
+    private String tokenUrl;
+}
