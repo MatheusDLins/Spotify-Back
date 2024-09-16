06a54c1032a34de8997b55d143f505e907abedfe
MatheusDLins <matheus934qwer@gmail.com>
2024-09-15 21:38:07 -0300

    feat: add AlbumDto and SpotifyApiClient for Spotify API integration

---
+++ backend/src/main/java/com/example/spotify/dto/AlbumDto.java
@@ -0,0 +1,30 @@
+package com.example.spotify.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import se.michaelthelin.spotify.enums.AlbumType;
+import se.michaelthelin.spotify.enums.ModelObjectType;
+import se.michaelthelin.spotify.model_objects.specification.ArtistSimplified;
+import se.michaelthelin.spotify.model_objects.specification.ExternalUrl;
+import se.michaelthelin.spotify.model_objects.specification.Image;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AlbumDto {
+
+    private AlbumType albumType; //
+    private ArtistSimplified[] artist; //vai retornar uma lista dos artistas
+    private ExternalUrl externalUrl;
+    private String id;
+    private Image[] images;
+    private String name;
+    private String releaseData;
+    private ModelObjectType type;
+    private BigDecimal value; //aqui vamos simular o valor dos albuns
+    }
